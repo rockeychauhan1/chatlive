fa1e51199bb230a969294eb73458560b2e5592bd
unknown <chauhan786rockey@gmail.com>
2023-12-17 23:09:38 +0530

    go

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ io.on("connection", (socket) => {
 // server.listen(3001, () => {
 //   console.log("SERVER RUNNING");
 // });
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
